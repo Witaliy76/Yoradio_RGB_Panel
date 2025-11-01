f358c00879e39a3ea846318587e123b99237c7c1
Vitaly Plaksin <Witaliy76vit@yandex.ru>
2025-11-01 22:21:54 +0500

    chore(theme,options): sync settings for UEDX48480021

--- src/mytheme.h
+++ src/mytheme.h
@@ -1,53 +1,61 @@
+//----------------------------------------------------------------------------------------------------------------
+//    This file was generated on the website https://vip-cxema.org/
+//    Program version: 1.2.0_03.06.2025
+//    File last modified: 22:10 01.11.2025
+//----------------------------------------------------------------------------------------------------------------
+//    Project home       https://github.com/e2002/yoradio
+//    Wiki               https://github.com/e2002/yoradio/wiki
+//    Описание на 4PDA   https://4pda.to/forum/index.php?s=&showtopic=1010378&view=findpost&p=112992611
+//    Как это прошить?   https://4pda.to/forum/index.php?act=findpost&pid=112992611&anchor=Spoil-112992611-2
+//----------------------------------------------------------------------------------------------------------------
 #ifndef _my_theme_h
 #define _my_theme_h
-
-/*
-    Тематика цветных дисплеев
-    DSP_ST7735, DSP_ST7789, DSP_ILI9341, DSP_GC9106, DSP_ILI9225, DSP_ST7789_240
-    ***********************************************************************
-    *    !!! Этот файл должен находиться в корневом каталоге скетча !!!    *
-    ***********************************************************************
-    Раскомментируйте (удалите двойную косую черту //) из нужной строки, чтобы применить цвет.
-*/
+//----------------------------------------------------------------------------------------------------------------
+//    Theming of color displays
+//    DSP_ST7735, DSP_ST7789, DSP_ILI9341, DSP_GC9106, DSP_ILI9225, DSP_ST7789_240
+//----------------------------------------------------------------------------------------------------------------
+//    *    !!! This file must be in the root directory of the sketch !!!    *
+//----------------------------------------------------------------------------------------------------------------
+//    Uncomment (remove double slash //) from desired line to apply color
+//----------------------------------------------------------------------------------------------------------------
 #define ENABLE_THEME
 #ifdef  ENABLE_THEME
-
 /*----------------------------------------------------------------------------------------------------------------*/
 /*       | COLORS             |   values (0-255)  |                                                               */
 /*       | color name         |    R    G    B    |                                                               */
 /*----------------------------------------------------------------------------------------------------------------*/
-#define COLOR_BACKGROUND        0, 0,  0     /*  фон                                               */
-#define COLOR_STATION_NAME        2, 84, 0     /*  Название станции                                              */
-#define COLOR_STATION_BG          140, 159, 255     /*  Фон Названия станции                                   */
-#define COLOR_STATION_FILL        140, 159, 255     /*  Фон заливки названия станции                              */
-#define COLOR_SNG_TITLE_1         255, 255, 255     /*  Первое название                                               */
-#define COLOR_SNG_TITLE_2         227,227,227     /*  Второе название                                             */
-#define COLOR_WEATHER             255,   0, 216     /*  строка погоды                                            */
-#define COLOR_VU_MAX              255, 0, 0     /*  max of VU meter - яркий красный */
-#define COLOR_VU_MIN              41, 189, 207     /*  min of VU meter - яркий зеленый */
-#define COLOR_CLOCK               41, 255, 55     /*  цвет часов                                               */
-#define COLOR_CLOCK_BG            0,  0,   0     /*  цвет фона часов                                    */
-#define COLOR_SECONDS             41, 255, 55     /*  цвет секунд (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)      */
-#define COLOR_DAY_OF_W            255,   255,   255     /*  цвет дня недели (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)  */
-#define COLOR_DATE                255,   255,   255     /*  цвет даты (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)         */
-#define COLOR_HEAP              255, 168, 162     /*  Строка кучи                                               */
-#define COLOR_BUFFER            41, 255, 55     /*  buffer line                                               */
-#define COLOR_IP                 41, 189, 207     /*  ip address                                                */
-#define COLOR_VOLUME_VALUE      41, 189, 207     /*  Строка громкости (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)      */
-#define COLOR_RSSI              41, 189, 207     /*  rssi                                                      */
-#define COLOR_VOLBAR_OUT        255, 255, 0     /*  контур шкалы громкости                                        */
-#define COLOR_VOLBAR_IN         255, 236, 41     /*  заполнение шкалы громкости                                           */
-//#define COLOR_DIGITS            100, 100, 255     /*  громкость/номер станции                                   */
-#define COLOR_DIVIDER             255, 255, 0     /*  цвет разделителя (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)      */
-#define COLOR_BITRATE           255, 255, 0     /*  bitrate                                                   */
-//#define COLOR_PL_CURRENT          0,   0,   0     /*  текущий элемент плейлиста                                     */
-//#define COLOR_PL_CURRENT_BG      91, 118, 255     /*  фон текущего элемента плейлиста                          */
-//#define COLOR_PL_CURRENT_FILL    91, 118, 255     /*  фон заливки текущего элемента плейлиста                     */
-#define COLOR_PLAYLIST_0        255, 255, 255     /*  playlist string 0                                         */
-#define COLOR_PLAYLIST_1          170, 170, 170     /*  playlist string 1                                         */
-#define COLOR_PLAYLIST_2        140,   140, 140     /*  playlist string 2                                         */
-#define COLOR_PLAYLIST_3          90,   90, 90     /*  playlist string 3                                         */
-#define COLOR_PLAYLIST_4          60, 60, 60     /*  playlist string 4                                         */
+#define COLOR_BACKGROUND        0,0,0     /*  background                                                  */
+#define COLOR_STATION_NAME        255,255,255     /*  station name                                                */
+#define COLOR_STATION_BG         108,71,51     /*  station name background                                     */
+#define COLOR_STATION_FILL       108,71,51     /*  station name fill background                                */
+#define COLOR_SNG_TITLE_1       108,71,51     /*  first title                                                 */
+#define COLOR_SNG_TITLE_2         108,71,51    /*  second title                                                */
+#define COLOR_WEATHER           234,116,35     /*  weather string                                              */
+#define COLOR_VU_MAX            132,0,0     /*  max of VU meter                                             */
+#define COLOR_VU_MIN            16,86,36     /*  min of VU meter                                             */
+#define COLOR_CLOCK              255,255,255     /*  clock color                                                 */
+#define COLOR_CLOCK_BG           28,28,28     /*  clock color background                                      */
+#define COLOR_SECONDS             195,195,195     /*  seconds color (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)        */
+#define COLOR_DAY_OF_W          171,171,171     /*  day of week color (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)    */
+#define COLOR_DATE                140,140,140     /*  date color (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)           */
+#define COLOR_HEAP              255,168,162     /*  heap string                                                 */
+#define COLOR_BUFFER            41,255,55     /*  buffer line                                                 */
+#define COLOR_IP                 41,189,207    /*  ip address                                                  */
+#define COLOR_VOLUME_VALUE      41,189,207     /*  volume string (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)        */
+#define COLOR_RSSI              255,255,0     /*  rssi                                                        */
+#define COLOR_VOLBAR_OUT        118,4,13     /*  volume bar outline                                          */
+#define COLOR_VOLBAR_IN         118,4,13     /*  volume bar fill                                             */
+#define COLOR_DIGITS            255,255,255     /*  volume / station number                                     */
+#define COLOR_DIVIDER             255,255,255     /*  divider color (DSP_ST7789, DSP_ILI9341, DSP_ILI9225)        */
+#define COLOR_BITRATE           255,255,0     /*  bitrate                                                     */
+#define COLOR_PL_CURRENT          0,0,0     /*  playlist current item                                       */
+#define COLOR_PL_CURRENT_BG      231,211,90     /*  playlist current item background                            */
+#define COLOR_PL_CURRENT_FILL    231,211,90     /*  playlist current item fill background                       */
+#define COLOR_PLAYLIST_0        255,255,255     /*  playlist string 0                                           */
+#define COLOR_PLAYLIST_1        170,170,170     /*  playlist string 1                                           */
+#define COLOR_PLAYLIST_2        140,140,140     /*  playlist string 2                                           */
+#define COLOR_PLAYLIST_3          90,90,90     /*  playlist string 3                                           */
+#define COLOR_PLAYLIST_4          60,60,60     /*  playlist string 4                                           */
 
 
 #endif  /* #ifdef  ENABLE_THEME */
